df53b969fa69a462c70521d8c4a1150deb993d48
昱恒 <xiaojian.xj@antfin.com>
2021-05-07 14:33:39 +0800

    create table

--- server/server/meta/src/test/resources/sql/h2/create_table.sql
+++ server/server/meta/src/test/resources/sql/h2/create_table.sql
@@ -52,8 +52,16 @@ CREATE TABLE provide_data (
   UNIQUE KEY `uk_data_center_key` (`data_center`, `data_key`)
 );
 
-
-
+drop table if exists client_off_pods;
+CREATE TABLE `client_off_pods` (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `data_center` varchar(128) NOT NULL COMMENT '集群名称',
+  `address` varchar(1024) NOT NULL COMMENT 'address',
+  `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_data_center_address` (`data_center`, `address`)
+);
 
 
 
---
+++ server/store/jdbc/src/main/java/com/alipay/sofa/registry/jdbc/domain/ClientOffPodsDomain.java
@@ -0,0 +1,124 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2021 All Rights Reserved.
+ */
+package com.alipay.sofa.registry.jdbc.domain;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ *
+ * @author xiaojian.xj
+ * @version $Id: ClientOffPodsDomain.java, v 0.1 2021年04月27日 22:37 xiaojian.xj Exp $
+ */
+public class ClientOffPodsDomain {
+
+    /** primary key */
+    private long id;
+
+    /** local data center */
+    private String dataCenter;
+
+    /** local data center */
+    private String address;
+
+    /** create time */
+    private Date gmtCreate;
+
+    /** last update time */
+    private Timestamp gmtModify;
+
+    public ClientOffPodsDomain() {
+    }
+
+    /**
+     * Getter method for property <tt>id</tt>.
+     *
+     * @return property value of id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Setter method for property <tt>id</tt>.
+     *
+     * @param id value to be assigned to property id
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Getter method for property <tt>dataCenter</tt>.
+     *
+     * @return property value of dataCenter
+     */
+    public String getDataCenter() {
+        return dataCenter;
+    }
+
+    /**
+     * Setter method for property <tt>dataCenter</tt>.
+     *
+     * @param dataCenter value to be assigned to property dataCenter
+     */
+    public void setDataCenter(String dataCenter) {
+        this.dataCenter = dataCenter;
+    }
+
+    /**
+     * Getter method for property <tt>address</tt>.
+     *
+     * @return property value of address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Setter method for property <tt>address</tt>.
+     *
+     * @param address value to be assigned to property address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Getter method for property <tt>gmtCreate</tt>.
+     *
+     * @return property value of gmtCreate
+     */
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    /**
+     * Setter method for property <tt>gmtCreate</tt>.
+     *
+     * @param gmtCreate value to be assigned to property gmtCreate
+     */
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    /**
+     * Getter method for property <tt>gmtModify</tt>.
+     *
+     * @return property value of gmtModify
+     */
+    public Timestamp getGmtModify() {
+        return gmtModify;
+    }
+
+    /**
+     * Setter method for property <tt>gmtModify</tt>.
+     *
+     * @param gmtModify value to be assigned to property gmtModify
+     */
+    public void setGmtModify(Timestamp gmtModify) {
+        this.gmtModify = gmtModify;
+    }
+}
